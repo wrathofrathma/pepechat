a7bf1234680b693b16f1262c3464b3c7794ddaae
Rathma <rathma@kyso.dev>
2021-05-15 07:41:39 -0400

    Some quick scaffolding for how the index might look

---
+++ src/components/Header.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="flex flex-row w-full h-20 bg-green-400 p-2 absolute rounded-sm">
+        <div class="flex-row flex w-1/3">
+            <div class="h-16 w-16 rounded-full bg-blue-400 flex items-center justify-center">
+                Avatar
+            </div>
+            <div class="bg-gray-600 justify-center items-center flex p-1">
+                {{username}}
+            </div>
+        </div>
+        <div class="bg-gray-600 items-center justify-center flex w-1/3">
+            PEPE CHAT LOGO
+        </div>
+        <div class="w-1/3 flex justify-end">
+            <div class="bg-gray-600 flex justify-center items-center">
+                Settings
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+import {useStore} from "vuex";
+import {computed} from "vue";
+
+const store = useStore();
+
+const username = computed(() => store.state.username);
+</script>
--- src/views/Index.vue
+++ src/views/Index.vue
@@ -1,9 +1,29 @@
 <template>
-    <div>
-        Hello world
+    <div class="flex flex-col">
+        <top-bar></top-bar>
+        <!-- Main content container-->
+        <div class="flex p-4 justify-center mt-20 pt-8">
+            <!-- Rooms card -->
+            <div class="bg-gray-800 w-1/2 rounded-sm border-green-400 border-2">
+                <!-- Header -->
+                <div class="text-center border-b-2 border-green-400 text-lg">
+                    Active Pepechats
+                </div>
+                <!-- Rooms list -->
+                <div class="flex flex-col">
+                    <div v-for="n in 20" class="p-2" :class="[n % 2 === 0 ? 'bg-gray-700' : 'bg-gray-800']">
+                        Room {{n}}
+                    </div>
+                </div>
+                <!-- Create room -->
+                <div class="border-t-2 border-green-400 text-lg p-2">
+                    Create room form
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
 <script setup lang="ts">
-
+import TopBar from "@/components/Header.vue";
 </script>
