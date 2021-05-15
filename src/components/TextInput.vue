bcf9d6c6e7cd4774168730bf69edf3dcd82d931f
Rathma <rathma@kyso.dev>
2021-05-15 11:40:01 -0400

    Button & TextInput unstyled minimal components

---
+++ src/components/TextInput.vue
@@ -0,0 +1,30 @@
+<template>
+    <input 
+    class="p-1 bg-transparent h-full w-full"
+    :placeholder="placeholder"
+    :value="modelValue"
+    @input="onInput"
+    >
+</template>
+<script setup lang="ts">
+import {defineProps, defineEmit, useContext} from "vue";
+
+const props = defineProps({
+    placeholder: {
+        type: String,
+        required: false
+    },
+    modelValue: {
+        type: String,
+        required: false
+    }
+})
+
+defineEmit(["update:modelValue"])
+const {emit} = useContext();
+
+const onInput = (event: Event) => {
+    const {target} = event;
+    emit("update:modelValue", (target as HTMLInputElement).value);
+}
+</script>
---
+++ src/components/VButton.vue
@@ -0,0 +1,11 @@
+<template>
+    <button @click="$emit('click')"
+    class="focus:outline-none"
+    ><slot></slot></button>
+</template>
+
+<script setup lang="ts">
+import {defineEmit} from "vue";
+
+defineEmit(["click"])
+</script>
