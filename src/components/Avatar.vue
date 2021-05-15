8bf8ac78c76ff7f584ad299b8184eda708c1ee45
Rathma <rathma@kyso.dev>
2021-05-15 11:37:24 -0400

    Added a real avatar & fiddled with the look of the header.

---
+++ src/components/Avatar.vue
@@ -0,0 +1,15 @@
+<template>
+    <img :src="src" class="w-16 h-16 rounded-full">
+</template>
+
+<script setup lang="ts">
+import {defineProps} from "vue";
+
+const props = defineProps({
+    src: {
+        type: String,
+        required: false,
+        default: "/src/assets/avatars/pepejam.gif"
+    }
+})
+</script>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,25 +1,30 @@
 <template>
-    <div class="flex flex-row w-full h-20 bg-green-400 p-2 absolute rounded-sm">
-        <div class="flex-row flex w-1/3">
-            <div class="h-16 w-16 rounded-full bg-blue-400 flex items-center justify-center">
-                Avatar
-            </div>
-            <div class="bg-gray-600 justify-center items-center flex p-1">
+    <div class="flex flex-row w-full h-20 bg-gray-800 p-2 absolute rounded-sm border-green-400 border-2">
+        <div class="flex-row flex w-1/3 space-x-2">
+            <v-button class="focus:outline-none">
+                <avatar class="border border-green-400"></avatar>
+            </v-button>
+            <div class="justify-center items-center flex p-1">
                 {{username}}
             </div>
         </div>
-        <div class="bg-gray-600 items-center justify-center flex w-1/3">
-            PEPE CHAT LOGO
+        <div class="items-center justify-center flex w-1/3 font-bold text-3xl text-green-400 cursor-default select-none">
+            PEPE CHAT
         </div>
-        <div class="w-1/3 flex justify-end">
-            <div class="bg-gray-600 flex justify-center items-center">
-                Settings
-            </div>
+        <div class="w-1/3 flex justify-end items-center">
+            <v-button class="hover:bg-gray-600 hover:bg-opacity-10 p-2 rounded-full focus:outline-none">
+                <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
+                </svg>
+            </v-button>
         </div>
     </div>
 </template>
 
 <script setup lang="ts">
+import Avatar from "@/components/Avatar.vue";
+import VButton from "@/components/VButton.vue"
+
 import {useStore} from "vuex";
 import {computed} from "vue";
 
