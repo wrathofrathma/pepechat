611bce2a27fc06a03ef97965977c2c8f4a4c7aa3
Rathma <rathma@kyso.dev>
2021-05-17 10:24:21 -0400

    Abstracted the overall layout to its own file so we can reuse it on other views

---
+++ src/layouts/Default.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="flex flex-col">
+        <top-bar></top-bar>
+        <!-- Main content container-->
+        <div class="flex" style="height: calc(100vh - 5rem);">
+            <slot></slot>
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+import TopBar from "@/components/Header.vue";
+</script>
--- src/views/Index.vue
+++ src/views/Index.vue
@@ -1,16 +1,12 @@
 <template>
-    <div class="flex flex-col">
-        <top-bar></top-bar>
-        <!-- Main content container-->
-        <div class="flex" style="height: calc(100vh - 5rem);">
-            <room-list class="w-5/6"></room-list>
-            <user-list class="w-1/6"></user-list>
-        </div>
-    </div>
+    <layout>
+        <room-list class="w-5/6"></room-list>
+        <user-list class="w-1/6"></user-list>
+    </layout>
 </template>
 
 <script setup lang="ts">
-import TopBar from "@/components/Header.vue";
+import Layout from "@/layouts/Default.vue";
 import RoomList from "@/components/RoomIndex/RoomList.vue"
 import UserList from "@/components/UserList.vue"
 </script>
