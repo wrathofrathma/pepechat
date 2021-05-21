340e244cd224ffcf6791c6af14f2e58bd19184cc
Rathma <rathma@kyso.dev>
2021-05-21 08:36:57 -0400

    Stream component, for now. Going to change it to initialize the webrtc connection

---
+++ src/components/Stream.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="flex flex-col w-96 h-96 bg-blue-400">
+        <video :autoplay="true" class="video" :srcObject="stream"></video>
+        {{streamUser}}
+        {{streamType}}
+    </div>
+</template>
+
+<script setup lang="ts">
+import {defineProps, computed, onMounted, onUnmounted} from "vue";
+
+import {useStore} from "vuex";
+import store from "../store";
+import {getUserWebcam} from "../scripts/streams";
+
+const props = defineProps({
+    streamUser: {
+        type: String,
+        required: true
+    },
+    streamType: {
+        type: String,
+        required: true
+    }
+})
+
+const stream = computed(() => store.getters.webcamStream(props.streamUser));
+
+const fetchStream = async () => {
+    if (!stream) 
+        await getUserWebcam();
+}
+
+let fetchInterval: number;
+
+onMounted(async () => {
+    fetchInterval = setInterval(fetchStream, 30 * 1000);
+    await fetchStream();
+})
+
+onUnmounted(() => {
+    clearInterval(fetchInterval);
+})
+</script>
