72080cfb61c09e23068f9ba33015483ddef8f0cb
Rathma <rathma@kyso.dev>
2021-05-21 08:33:48 -0400

    Media control button for toggling webcam/screenshare/mic

---
+++ src/components/Chatroom/MediaControlButton.vue
@@ -0,0 +1,35 @@
+<template>
+    <v-button 
+    class="p-2 rounded-full hover:brightness-125 filter hover:bg-opacity-10 hover:bg-green-400" 
+    :class="mediaButtonClasses(active)"
+    @click="$emit('click')"
+    >
+        <slot></slot>
+    </v-button>
+</template>
+
+<script setup lang="ts">
+import {defineProps, defineEmit} from "vue";
+import VButton from "@/components/atomic/VButton.vue"
+
+const props = defineProps({
+    active: {
+        type: Boolean,
+        default: false
+    }
+})
+
+defineEmit(["click"]);
+
+const mediaButtonClasses = (active: boolean) => {
+    const activeClasses = [
+        "text-white"
+    ];
+
+    const inactiveClasses = [
+        "text-gray-400"
+    ];
+
+    return active ? activeClasses : inactiveClasses;
+}
+</script>
