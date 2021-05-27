238081551826073d2590d8ffec3d4dfb34eba49a
Rathma <rathma@kyso.dev>
2021-05-27 19:18:24 -0400

    If there are no active video streams, then the streaming container is hidden.

--- src/components/Chatroom/Room.vue
+++ src/components/Chatroom/Room.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="flex flex-col">
-        <room-stream-container></room-stream-container>
+        <room-stream-container :class="[nVideos === 0 ? 'hidden' : '']"></room-stream-container>
         <message-list :room-id="roomId"></message-list>
         <create-message class="border-green-400 border-t border-b" :room-id="roomId"></create-message>
     </div>
@@ -9,8 +9,11 @@
 <script setup lang="ts">
 import CreateMessage from "@/components/CreateMessage.vue";
 import MessageList from "./MessageList.vue";
-import {defineProps} from "vue";
+import {computed, defineProps} from "vue";
+import {useStore} from "vuex";
 import RoomStreamContainer from "./RoomStreamContainer.vue"
+
+const store = useStore();
 
 const props = defineProps({
     roomId: {
@@ -19,4 +22,5 @@ const props = defineProps({
     }
 });
 
+const nVideos = computed(() => store.getters.numberOfVideoStreams(props.roomId));
 </script>
